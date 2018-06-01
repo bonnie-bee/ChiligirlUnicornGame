334235d6ad36e39bfcac9a668525700318e5ea4e
notsilverman <birchba@gmail.com>
2018-06-01 00:40:55 -0500

    Finished all the question sections, still nested in each click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,11 +12,27 @@ class Answers{
 }
 
 
+//Set chiligirl function
+function chiligirl(){
+    $('.game').html("<h1>You're a chiligirl! Hopefully tomorrow I remember to set this to an image screen with the html image tag here instead of this phrase</h1>")
+}
+
+
+//set unicorn function
+function unicorn(){
+    $('.game').html("<h1>You're a unicorn! Hopefully tomorrow I remember to set this to an image screen with the html image tag here instead of this phrase</h1>")
+}
+
+//set chilicorn function
+function chilicorn(){
+    $('.game').html("<h1>Gadzooks! You're the elusive chilicorn! Hopefully tomorrow I remember to set this to an image screen with the html image tags here instead of this phrase</h1>")
+}
+
 //Press start button to start game
 $("button").on("click", function(){
 
 
-//Replace starting html and show question
+//Replace starting html and show question 1
     $(".game").html("<h1>Where would you rather live?</h1>")
 
 
@@ -33,27 +49,87 @@ $("button").on("click", function(){
         console.log("first: " + total)
 
 
-//I seem to have coded myself into a corner - Make a new question
-     $(".game").html("<h1>Beans in chili?</h1>")
+//I seem to have coded myself into a corner - Make a new question 2
+        $(".game").html("<h1>Beans in chili?</h1>")
 
 
-
-     //Put in answers with different values
-         let answer5 = new Answers(1, 'ans5');
-         let answer6 = new Answers(2, 'ans6');
+//Put in answers with different values
+        let answer5 = new Answers(1, 'ans5');
+        let answer6 = new Answers(2, 'ans6');
          
          
 //When clicked, add the answer values to the total, log the total to make sure it's working  
-         $(".answer").on("click", function(){
-             total += parseInt($(this).attr('value'));
-             console.log("second: " + total)
-     })
+        $(".answer").on("click", function(){
+            total += parseInt($(this).attr('value'));
+            console.log("second: " + total)
 
-     
 
-     })
+//Still in it - Make a new question 3
+            $(".game").html("<h1>What kind of chili do you want?</h1>")
 
- });
+
+//Put in answers with different values
+            let answer7 = new Answers(1, 'ans7');
+            let answer8 = new Answers(0, 'ans8');
+            let answer9 = new Answers(-1, 'ans9');
+                
+                
+//When clicked, add the answer values to the total, log the total to make sure it's working  
+            $(".answer").on("click", function(){
+                total += parseInt($(this).attr('value'));
+                console.log("second: " + total)
+
+//Still in it - Make a new question 4
+                $(".game").html("<h1>What position do you hold on your chili team?</h1>")
+
+
+//Put in answers with different values
+                let answer10 = new Answers(1, 'ans10');
+                let answer11 = new Answers(0, 'ans11');
+                let answer12 = new Answers(-1, 'ans12');
+                let answer13 = new Answers(-1, 'ans13');
+                let answer14 = new Answers(-1, 'ans14');
+                    
+                    
+//When clicked, add the answer values to the total, log the total to make sure it's working  
+                $(".answer").on("click", function(){
+                    total += parseInt($(this).attr('value'));
+                    console.log("second: " + total)
+                    
+                        
+//Last one - Make a new question 5
+                    $(".game").html("<h1>Who's your best friend?</h1>")
+
+
+//Put in answers with different values
+                    let answer15 = new Answers(2, 'ans15');
+                    let answer16 = new Answers(-2, 'ans16');
+                        
+                        
+//When clicked, add the answer values to the total, log the total to make sure it's working  
+                    $(".answer").on("click", function(){
+                        total += parseInt($(this).attr('value'));
+                        console.log("second: " + total)      
+                        
+                        if (total>0){
+                            chiligirl()
+                        } else if(total<0){
+                            unicorn()
+                        } else if(total===0){
+                            chilicorn()
+                        }
+                        
+                        
+
+                    })
+                })    
+            })
+        })
+    })
+});
+
+
+        
 
  
 
